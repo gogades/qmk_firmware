99b5694c952dff40d6608d3a2e2cdcb7cebaaa66
Phil Tanguay <phil.tanguay@oracle.com>
2019-10-13 11:26:33 -0400

    got it working with the behaviour I want! woohoo!

--- keyboards/massdrop/alt/keymaps/ptanguay/keymap.c
+++ keyboards/massdrop/alt/keymaps/ptanguay/keymap.c
@@ -25,17 +25,20 @@
 #define _MAC     1
 #define _FUNC    2
 #define _FUNCALT 3
+#define _RGB     4
 
 enum alt_keycodes {
     MD_BOOT = SAFE_RANGE, //Restart into bootloader after hold timeout
     WINDOWS,
-    MAC
+    MAC,
+    KC_FN
 };
 
 #define TG_NKRO MAGIC_TOGGLE_NKRO //Toggle 6KRO / NKRO mode
 
 uint8_t offset = 0;
 uint8_t current_layer=0;
+uint8_t previous_layer=0;
 
 keymap_config_t keymap_config;
 
@@ -46,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(2,KC_TAB),         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN , \
         MT(MOD_LCTL,KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_DEL, \
         KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   LT(3,KC_END), \
-        KC_LCTL,              KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(2),   KC_LEFT, KC_DOWN, KC_RGHT  \
+        KC_LCTL,              KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_FN,   KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     /* mac layout */
     /* so far we only swap lctl and gui */
@@ -55,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(2,KC_TAB),         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN , \
         MT(MOD_LCTL,KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_DEL, \
         KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   LT(3,KC_END), \
-        KC_LGUI,              KC_LCTL, KC_LALT,                            KC_SPC,                             KC_RALT, MO(2),   KC_LEFT, KC_DOWN, KC_RGHT  \
+        KC_LGUI,              KC_LCTL, KC_LALT,                            KC_SPC,                             KC_RALT, KC_FN,   KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [_FUNC] = LAYOUT(
         KC_GRV,               KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,      KC_F9,     KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
@@ -63,6 +66,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,              RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, DF(_WINDOWS),DF(_MAC), _______, _______,          _______, KC_INS, \
         _______,              RGB_TOG, _______, _______, _______, RESET  , TG_NKRO, _______, _______,    _______,   _______, _______,          KC_PGUP, KC_HOME, \
         _______,              _______, _______,                            _______,                                 _______, _______, KC_HOME, KC_PGDN, KC_END  \
+    ),
+    [_RGB] = LAYOUT(
+        _______,              _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______,              RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______,              RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______,          _______, _______, \
+        _______,              RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
+        _______,              _______, _______,                            _______,                            _______, _______, _______, _______, _______  \
     ),
     [_FUNCALT] = LAYOUT(
         _______,              _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_ESC , \
@@ -90,7 +100,7 @@ struct hsv {
     uint8_t h;
     uint8_t s;
     uint8_t v;
-} layer_color[2];
+} layer_color[4];
 
 void set_layer_color(void) {
     rgblight_sethsv(layer_color[current_layer].h,
@@ -109,35 +119,49 @@ void keyboard_post_init_user(void) {
     layer_color[_MAC].s = 0;
     layer_color[_MAC].v = 255;
 
+    layer_color[_FUNC].h= 43;
+    layer_color[_FUNC].s = 255;
+    layer_color[_FUNC].v = 255;
+
+    layer_color[_FUNCALT].h= 85;
+    layer_color[_FUNCALT].s = 255;
+    layer_color[_FUNCALT].v = 255;
+
     current_layer = _WINDOWS;
     set_layer_color();
-
 }
 
-void calc(uint8_t offset) {
-    int tmp = layer_color[current_layer].v + offset;
-    if(tmp > 255) {
-        tmp = 255;
-    } else if (tmp < 0) {
-        tmp =0;
-    }
-    layer_color[current_layer].v = tmp;
+void save_val(uint8_t offset) {
+    layer_color[current_layer].v = rgblight_get_val();
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint32_t key_timer;
 
     switch (keycode) {
+        case KC_FN:
+            if(record->event.pressed && MODS_ALT ) {
+                layer_on(_RGB);
+            } else 
+            if (record->event.pressed) {
+                layer_on(_FUNC);
+                previous_layer = current_layer;
+                current_layer = _FUNC;
+                set_layer_color();
+            } else {
+                layer_off(_FUNC);
+                layer_off(_RGB);
+                current_layer = previous_layer;
+                set_layer_color();
+            }
+            return false;
+            break;
         case RGB_VAI:
-            calc(RGBLIGHT_VAL_STEP);
-            set_layer_color();
-            return false;
+            save_val(RGBLIGHT_VAL_STEP);
+            return true;
         case RGB_VAD:
-            calc(0-RGBLIGHT_VAL_STEP);
-            rgblight_increase_val();
-
-            set_layer_color();
-            return false;
+            save_val(0-RGBLIGHT_VAL_STEP);
+            return true;
         case MD_BOOT:
             if (record->event.pressed) {
                 key_timer = timer_read32();
@@ -153,34 +177,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-/*    switch(get_highest_layer(state))
-    {
-    case _WINDOWS:
-    case _MAC:
-	if(biton32(default_layer_state) == 0)
-		rgblight_set_teal
-	else
-		rgblight_set_white
+    switch(get_highest_layer(state)){
+        case _FUNCALT:
+            previous_layer = current_layer;
+            current_layer = _FUNCALT;
+            set_layer_color();
         break;
-    case _FUNC:
-        rgblight_set_yellow
+        default:
+            current_layer = previous_layer;
+            set_layer_color();
         break;
-    case _FUNCALT:
-        rgblight_set_green
-        break;
-    default: //  for any other layers, or the default layer
-        rgblight_set_white
-        break;
-    } */
-  return state;
+    }
+    return state;
 }
 
 uint32_t default_layer_state_set_kb(uint32_t state) {
-
-    current_layer= biton32(state);
+    // switching default layer
+    current_layer = previous_layer =  biton32(state);
     set_layer_color();
-
-   return state;
+    return state;
 }
-
-
