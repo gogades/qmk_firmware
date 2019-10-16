8f4eac5b28c0f1a4b218f4f3422a314c1c6cb3cf
phil <gogades@gmail.com>
2019-10-16 07:52:12 -0400

    set default keymap flip flop

--- keyboards/massdrop/alt/keymaps/ptanguay/keymap.c
+++ keyboards/massdrop/alt/keymaps/ptanguay/keymap.c
@@ -273,9 +273,11 @@ void keyboard_post_init_user(void) {
     layer_color[_FUNCALT].s = 255;
     layer_color[_FUNCALT].v = 255;
 
-    current_layer = _MAC;
-    if(eeconfig_read_default_layer() != current_layer)
-        set_single_persistent_default_layer(current_layer);
+    if(eeconfig_read_default_layer() == _WINDOWS)
+        current_layer=_MAC;
+    else
+        current_layer=_WINDOWS;
+    set_single_persistent_default_layer(current_layer);
     set_layer_color();
 }
 
